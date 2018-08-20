f74104cc3deb44e38458847a4296d5de74ee1026
Michal Vašek <Michal.Vasek@firma.seznam.cz>
2018-08-20 14:01:38 +0200

    Selenium tests

---
+++ test/browser/notification_toast/test.py
@@ -0,0 +1,20 @@
+import time
+import os
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_argument("nwapp=" + os.path.dirname(os.path.abspath(__file__)) + "\..\javascript")
+
+driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_options=chrome_options)
+time.sleep(1)
+try:
+    driver.implicitly_wait(10) # 10s timeout when finding an element
+
+    clickme = driver.find_element_by_id('notificationToastButton')
+    clickme.click() 
+
+    result = driver.find_element_by_id('notificationToastResult')
+    assert("success" in result.get_attribute('innerHTML'))
+finally:
+    driver.quit()
