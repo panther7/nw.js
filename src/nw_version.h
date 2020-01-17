6db9a04491a5aedabcae12b99db5216deceb615c
Michal Vašek <Michal.Vasek@firma.seznam.cz>
2020-01-17 12:10:28 +0100

    Seznam suffix in version

--- src/nw_version.h
+++ src/nw_version.h
@@ -35,7 +35,7 @@
 #if NW_VERSION_IS_RELEASE
 # define NW_VERSION_STRING  NW_STRINGIFY(NW_MAJOR_VERSION) "." \
                               NW_STRINGIFY(NW_MINOR_VERSION) "." \
-                              NW_STRINGIFY(NW_PATCH_VERSION)
+                              NW_STRINGIFY(NW_PATCH_VERSION) "+szn.1"
 #else
 # define NW_VERSION_STRING  NW_STRINGIFY(NW_MAJOR_VERSION) "." \
                               NW_STRINGIFY(NW_MINOR_VERSION) "." \
