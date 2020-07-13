2675ae9fdb3aab480cce459abf94bc86c1bb7d08
Michal Vašek <Michal.Vasek@firma.seznam.cz>
2020-07-13 15:53:43 +0200

    ADD: CI support - split into build stage and deploy stage

--- build/win/pt2_build.bat
+++ build/win/pt2_build.bat
@@ -1,3 +1,15 @@
-REM Build files
+REM Build nwjs files
 cd C:\nwjs\src
 call ninja -C out/nw nwjs
+
+REM Generate node ninja files
+cd C:\nwjs\src
+call python ./build/gyp_chromium -D target_arch=ia32 -D building_nw=1 -D icu_use_data_file_flag=1 -D clang=1 -D nwjs_sdk=1 -D disable_nacl=0 -I third_party/node-nw/common.gypi third_party/node-nw/node.gyp
+
+REM Build node files
+cd C:\nwjs
+call ninja -C src/out/Release node
+
+REM Copy node files
+cd C:\nwjs\src
+call ninja -C out/nw copy_node
--- build/win/pt3_deploy.bat
+++ build/win/pt3_deploy.bat
@@ -2,5 +2,5 @@ REM Third portion of python scripts in virtual environment (Python 3)
 @echo off
 cd C:\nwjs
 call env\Scripts\activate
-python build\nw_build.py build\config.yaml pt3_build
+python build\nw_build.py build\config.yaml pt3_deploy
 call deactivate
