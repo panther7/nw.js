96ed9c1f94a12879c8cdbcec272e2f1f4682ceed
Martin Kodovský <martin.kodovsky@firma.seznam.cz>
2020-07-15 14:48:17 +0200

    MOD: Deploy pouziva syntetickou commit message

--- build/lin/deploy.sh
+++ build/lin/deploy.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+. ${SCRIPTDIR}/version.sh
+
 SEDCOMMANDE=(-rn)
 if [[ $OSTYPE == "darwin"* ]]; then
     SEDCOMMANDE=(-En)
@@ -9,8 +13,10 @@ fi
 
 RETVAL=0
 
-GITTAG=`git describe --tags`
-TITLE_MESSAGE=`git tag ${GITTAG} -n1 | cut -d' ' -f 2-`
+GITTAG=$(git describe --tags | grep -oE "[^-]+$")
+
+# e.g. NW.js v0.45.1+szn.1 (Node v13.12.0, Chromium 81.0.4044.92)
+TITLE_MESSAGE="NW.js $GITTAG (Node v$NODE_VER, Chromium $CHROMIUM_VER)"
 BODY_MESSAGE="${CI_COMMIT_SHORT_SHA}: ${CI_COMMIT_MESSAGE}
  - branch: ${CI_COMMIT_REF_NAME}
  - url: ${CI_PROJECT_URL}/-/commit/${CI_COMMIT_SHA}"
@@ -80,6 +86,11 @@ fi
 NWJSTAG=`echo ${ARTIFACT} | sed ${SEDCOMMANDE[@]} 's|.*(v[0-9]+\.[0-9]+\.[0-9]+).*|\1|p'` #e.g. browser-v0.45.2+szn.1 = v0.45.2
 NWJSMAJOR=`echo ${NWJSTAG} | sed ${SEDCOMMANDE[@]} 's|.*v[0-9]+\.([0-9]+)\.[0-9]+.*|\1|p'` #e.g. v0.45.2 = 45
 
+if [[ -z ${GITMAJOR} ]]; then
+    echo -E "\033[0;31m NWjs-build-* repository seems to be inconsistent. Please fix it manually. \033[0;37m"
+    exit 1
+fi
+
 echo -e "\033[0;36mCompare ${GITMAJOR} -lt ${NWJSMAJOR}\033[0;37m"
 if [[ ${GITMAJOR} -lt ${NWJSMAJOR} ]]; then
     GITBRANCH=browser${NWJSMAJOR}_dev
@@ -96,7 +107,7 @@ fi
 
 echo "Removing old versions in $PWD"
 
-find ./ -maxdepth 1 -type d -print | grep -E "v[0-9]\.[0-9]+\.[0-9]+$" | xargs "${XARGSCOMMAND[@]}" sh -c 'for arg do echo -e "\033[0;95m removing: $arg\033[0;37m"; git rm -r --cached $arg; rm -rf $arg; done' _
+find ./ -maxdepth 1 -type d -print | grep -E "v[0-9]\.[0-9]+\.[0-9]+$" | xargs "${XARGSCOMMAND[@]}" sh -c 'for arg do echo "\033[0;95m removing: $arg\033[0;37m"; git rm -r --cached $arg; rm -rf $arg; done' _
 
 echo "Unzipping ${ARTIFACT} ..."
 tar xfz ${ARTIFACT}
