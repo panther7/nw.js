d07631592bfce4a5090f24b6ed79d7f28de841ac
Filip Mösner <filip.mosner@firma.seznam.cz>
2020-07-16 14:58:36 +0200

    add: widevine for windows

--- build/win/pt1_build.bat
+++ build/win/pt1_build.bat
@@ -1,5 +1,11 @@
 REM First portion of python scripts in virtual environment (Python 3)
 @echo off
+
+REM Copy widevine
+rmdir C:\nwjs\build\WidevineCdm /s /q
+mkdir C:\nwjs\build\WidevineCdm
+xcopy WidevineCdm C:\nwjs\build\WidevineCdm /s /e
+
 cd C:\nwjs
 call env\Scripts\activate
 python build\nw_build.py build\config.yaml pt1_build
