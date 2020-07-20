ca349ee48bc6e95ade566b41e37bce63c36b42e0
Browser.Builder <sbrowser@sbuilder.cz>
2020-07-20 12:28:14 +0200

    CI: clean nwjs and node before build

--- build/win/pt1_build.bat
+++ build/win/pt1_build.bat
@@ -1,15 +1,13 @@
 REM First portion of python scripts in virtual environment (Python 3)
 @echo off
 
-REM Copy widevine
-rmdir C:\nwjs\build\WidevineCdm /s /q
-mkdir C:\nwjs\build\WidevineCdm
-xcopy WidevineCdm C:\nwjs\build\WidevineCdm /s /e
-
 cd C:\nwjs
+if %errorlevel% neq 0 exit /b %errorlevel%
 call env\Scripts\activate
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 python build\nw_build.py build\config.yaml pt1_build
-if %errorlevel% NEQ 0 exit /b %errorlevel%
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 call deactivate
+if %errorlevel% neq 0 exit /b %errorlevel%
