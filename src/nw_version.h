6636b73aa63a8fa723d4c938ce9f1aef6a01e7c0
Filip Mösner <filip.mosner@firma.seznam.cz>
2020-08-16 15:16:09 +0200

    Seznam suffix in version

--- src/nw_version.h
+++ src/nw_version.h
@@ -35,7 +35,7 @@
 #if NW_VERSION_IS_RELEASE
 # define NW_VERSION_STRING  NW_STRINGIFY(NW_MAJOR_VERSION) "." \
                               NW_STRINGIFY(NW_MINOR_VERSION) "." \
-                              NW_STRINGIFY(NW_PATCH_VERSION) "+szn.2"
+                              NW_STRINGIFY(NW_PATCH_VERSION) "+szn.3"
 #else
 # define NW_VERSION_STRING  NW_STRINGIFY(NW_MAJOR_VERSION) "." \
                               NW_STRINGIFY(NW_MINOR_VERSION) "." \
